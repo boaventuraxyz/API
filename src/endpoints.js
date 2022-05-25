5fdc30b8f13873e7f9bcbfe770881bccf73a665e
Julio Boaventura <juliospinola.l@gmail.com>
2022-05-25 00:45:35 -0300

    aaa

---
+++ src/endpoints.js
@@ -0,0 +1,185 @@
+import { Router } from 'express'
+import { Primaria, Cinema, Freq, Maior, Tabuada, Febre, Dobro, Soma, Media } from './services.js'
+
+const server = Router();
+
+
+server.get('/primaria/:cor' , (req, resp) => {
+    try{
+        const a = req.params.cor;
+
+        const x = Primaria(a);
+
+        resp.send({
+            primaria: x
+        })
+    }
+    catch(err) {
+        resp.send({
+            erro: err.message
+        })
+    }
+})
+
+server.post('/cinema' , (req, resp) => {
+    try{
+
+        const {a,b,c} = req.body
+
+        const y = Cinema(a,b,c)
+
+    resp.send({
+        cinema: y
+    })
+}
+    catch(err) {
+        resp.send({
+            erro: err.message
+        })
+    }
+
+})
+
+server.get('/freq/:frase/:c' , (req, resp) =>{
+    try{
+        const {frase, c} = req.params;
+
+        const x = Freq(frase, c)
+
+        resp.send({
+            Numero: x
+        })
+    }
+    catch(err){
+        resp.send({
+            erro: err.message
+        }) 
+    }
+
+})
+
+server.post('/maior' , (req,resp) => {
+    try{
+        const n = req.body.n
+
+        const y = Maior(n)
+
+        resp.send({
+            maior: y 
+        })
+
+    }
+    catch(err){
+        resp.send({
+            erro : err.message 
+        })
+    }
+})
+
+server.get('/tabuada' , (req, resp) => {
+    try{
+    const a = req.query.n
+
+     const x = Tabuada(a)
+
+     resp.send({
+         tabuada: x
+     })
+
+}
+    catch(err){
+        resp.send({
+            erro: err.message 
+        })
+    }
+})
+
+server.get('/febre' , (req, resp) => {
+    try{
+    
+    const g = req.query.g
+
+    const x = Febre(g) 
+
+    resp.send({
+        febre: x 
+    })
+}
+    catch(err) {
+        resp.send({
+            erro: err.message 
+        })
+    }
+})
+
+server.get('/dobro/:n' , (req, resp) => {
+    try{
+    const n = req.params.n
+
+    const x = Dobro(n) 
+    resp.send ({
+        dobro: x
+    })
+}
+    catch(err){
+        resp.send({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/soma' , (req,resp) => {
+    try{
+    //const {n1, n2} = Number(req.query) 
+
+    const n1 = Number(req.query.n1)
+    const n2 = Number(req.query.n2)
+
+    const x = Soma(n1,n2) 
+
+    resp.send({
+        soma: x 
+    })
+}
+    catch(err){
+        resp.send({
+            erro: err.message
+        })
+    }
+})
+
+server.post('/soma' , (req,resp) => {
+    try{
+    const {n1, n2} = req.body 
+
+    const x = Soma(n1,n2) 
+
+    resp.send({
+        soma: x 
+    })
+}
+    catch(err){
+        resp.send({
+            erro: err.message
+        })
+    } 
+})
+
+server.post('/media' , (req,resp) => {
+    try{
+    const {n1,n2,n3} = req.body
+
+    const x = Media (n1,n2,n3)
+
+    resp.send({
+        média: x
+    })
+}
+    catch(err) {
+        resp.send({
+            erro: err.message
+        }) 
+    }
+})
+
+export default server;
---
+++ src/services.js
@@ -0,0 +1,84 @@
+
+export function Primaria(cor){
+    if(cor == "azul" ||cor == "vermelho" ||cor == "amarelo"){
+        return true
+    }
+    else {
+        return false
+    }
+
+}
+export function Cinema (filme, dia, pessoas){
+    let i = 0;
+    if (filme == 'nacional' && dia == 'quarta'){
+        i = 2.50;
+    }
+    else if (filme == 'nacional'){
+        i = 5;
+    }
+    else if (dia == 'quarta' && filme == 'internacional'){
+        i = 14.25;
+    }
+    else if (filme == 'internacional'){
+        i = 28.50;
+    }
+
+    let x = i * pessoas;
+    return x;
+}
+
+export function Freq (texto, caracter){    
+
+    let a = 0;
+
+    for(let i = 0; i < texto.length; i++){
+        if (texto.charAt(i) == caracter) {
+            a++
+        }
+    }
+
+    return a;
+}
+
+export function Maior (n){
+    let x= 0;
+    for(let item of n){
+
+        if ( x < item){
+            x = item; 
+        } 
+    }
+    return x;
+}
+
+export function Tabuada (n){
+    let x = []
+    let e = 0;
+    for (let i = 1; i <= 10; i++){
+        x[e] = i * 5
+        e++ 
+    }
+    return x
+
+}
+
+export function Febre(g){
+    let x = false
+    if (g >= 38){
+        x = true 
+    }
+    return x
+}
+
+export function Dobro (n){
+    return n * 2
+}
+export function Soma (n1, n2){
+    return n1 + n2
+}
+
+export function Media(n1,n2,n3){
+    let x = n1 + n2 + n3
+
+    return x/3  
+}
